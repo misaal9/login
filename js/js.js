9a77c111edeb0e361e342bfe6a9528ef69a0f644
Rohit Diwakar <rohit.diwakar@snstech.com>
2015-03-19 00:13:16 +0530

    Created landing, added styles.

--- js/js.js
+++ js/js.js
@@ -1,4 +1,25 @@
-$(document).ready(function(){
-	
 
-});
+function signinCallback(authResult) {
+	if (authResult['status']['signed_in']) {
+		// Update the app to reflect a signed in user
+		// Hide the sign-in button now that the user is authorized, for example:
+		document.getElementById('signinButton').setAttribute('style', 'display: none');
+		console.log(authResult);
+
+		showLanding();
+
+	} else {
+		// Update the app to reflect a signed out user
+		// Possible error values:
+		//   "user_signed_out" - User is signed-out
+		//   "access_denied" - User denied access to your app
+		//   "immediate_failed" - Could not automatically log in the user
+		console.log('Sign-in state: ' + authResult['error']);
+	}
+}
+
+function showLanding() {
+	$('#login').slideUp('fast', function(){
+		$('#landing').slideDown('fast');
+	});
+}
